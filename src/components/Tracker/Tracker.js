c6272ab9073b358a5a64e537dfe431e5cd89365d
HumaRukshan25 <1hk19cs061@hkbk.edu.in>
2023-12-29 16:07:13 +0530

    firebase Backend Apis and firebase Realtime Database.

--- src/components/Tracker/Tracker.js
+++ src/components/Tracker/Tracker.js
@@ -4,6 +4,7 @@ import fire from '../../config/Fire';
 import Transaction from './Transaction/Transaction';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import UserProfile from '../Forms/UserProfile';
+
 class Tracker extends Component {
   constructor(props) {
     super(props);
@@ -14,7 +15,28 @@ class Tracker extends Component {
       description: '',
       price: '',
       date: '',
+      selectedCategory: '',
     };
+
+    // Reference to Firebase Firestore collection
+    this.expensesCollection = fire.firestore().collection('expenses');
+  }
+
+  componentDidMount() {
+    // Load expenses from localStorage
+    const storedExpenses = JSON.parse(localStorage.getItem('expenses'));
+    if (storedExpenses) {
+      this.setState({ expenses: storedExpenses });
+    }
+
+    // Fetch expenses from Firebase Firestore
+    this.expensesCollection.onSnapshot((snapshot) => {
+      const expenses = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      this.setState({ expenses });
+
+      // Save expenses to localStorage
+      localStorage.setItem('expenses', JSON.stringify(expenses));
+    });
   }
 
   handleInputChange = (event) => {
@@ -22,39 +44,81 @@ class Tracker extends Component {
   };
 
   handleAddExpense = () => {
-    const { description, price, date, expenses } = this.state;
+    const { description, price, date, selectedCategory } = this.state;
 
-    if (description && price && date) {
+    if (description && price && date && selectedCategory) {
       const newExpense = {
-        id: expenses.length + 1,
         description,
         price,
         date,
+        category: selectedCategory,
       };
 
-      this.setState({
-        expenses: [...expenses, newExpense],
-        description: '',
-        price: '',
-        date: '',
-      });
+      // Add expense to Firebase Firestore
+      this.expensesCollection
+        .add(newExpense)
+        .then((docRef) => {
+          // Update local state with the new expense ID
+          this.setState(
+            (prevState) => ({
+              expenses: [
+                ...prevState.expenses,
+                {
+                  id: docRef.id,
+                  ...newExpense,
+                },
+              ],
+              description: '',
+              price: '',
+              date: '',
+              selectedCategory: '',
+            }),
+            // Save expenses to localStorage after updating state
+            () => {
+              localStorage.setItem('expenses', JSON.stringify(this.state.expenses));
+            }
+          );
+        })
+        .catch((error) => {
+          console.error('Error adding expense to Firestore:', error);
+        });
+
+      // POST request using Firebase Realtime Database REST API
+      fetch('https://expense-tracker-db3a0-default-rtdb.firebaseio.com/expenses.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newExpense),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('Success:', data);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
     }
   };
 
   handleEditExpense = (id, updatedExpense) => {
-    const { expenses } = this.state;
-    const updatedExpenses = expenses.map((expense) =>
-      expense.id === id ? { ...expense, ...updatedExpense } : expense
-    );
-
-    this.setState({ expenses: updatedExpenses });
+    // Update expense in Firebase Firestore
+    this.expensesCollection
+      .doc(id)
+      .update(updatedExpense)
+      .catch((error) => {
+        console.error('Error updating expense in Firestore:', error);
+      });
   };
 
   handleDeleteExpense = (id) => {
-    const { expenses } = this.state;
-    const updatedExpenses = expenses.filter((expense) => expense.id !== id);
-
-    this.setState({ expenses: updatedExpenses });
+    // Delete expense from Firebase Firestore
+    this.expensesCollection
+      .doc(id)
+      .delete()
+      .catch((error) => {
+        console.error('Error deleting expense from Firestore:', error);
+      });
   };
 
   getTotalExpenses = () => {
@@ -70,15 +134,15 @@ class Tracker extends Component {
     fire.auth().signOut();
   };
 
-  // tracker.js
-
-
   render() {
-    const { currentUser, expenses, description, price, date } = this.state;
+    const { currentUser, expenses, description, price, date, selectedCategory } = this.state;
 
-    return ( 
+    // Sample list of expense categories
+    const expenseCategories = ['Food', 'Petrol', 'Salary', 'Other'];
+
+    return (
       <div className="container mt-5 lavender-background">
-           <UserProfile/>
+        <UserProfile />
         <div className="welcome bg-light p-3 ">
           <span>Hi, {currentUser.displayName}! Welcome to Expense Tracker</span>
           <button className="btn btn-danger ml-2" onClick={this.logout}>
@@ -107,19 +171,33 @@ class Tracker extends Component {
                 onChange={this.handleInputChange}
               />
             </div>
-            <div className="col-3">
-  <input
-    type="date"
-    className="form-control"
-    name="date"
-    value={date}
-    onChange={this.handleInputChange}
-  />
-
+            <div className="col-2">
+              <select
+                className="form-control"
+                name="selectedCategory"
+                value={selectedCategory}
+                onChange={this.handleInputChange}
+              >
+                <option value="">Select Category</option>
+                {expenseCategories.map((category) => (
+                  <option key={category} value={category}>
+                    {category}
+                  </option>
+                ))}
+              </select>
             </div>
             <div className="col-2">
+              <input
+                type="date"
+                className="form-control"
+                name="date"
+                value={date}
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <div className="col-1">
               <button className="btn btn-primary" onClick={this.handleAddExpense}>
-                Add Expense
+                Add
               </button>
             </div>
           </div>
@@ -138,7 +216,6 @@ class Tracker extends Component {
           <div>Total Expenses: {this.getTotalExpenses()}</div>
           <div>Total Price: ${this.getTotalPrice()}</div>
         </div>
-     
       </div>
     );
   }
